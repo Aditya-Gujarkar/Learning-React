639255834058305dcdd93f63a986fca27202e4c9
Aditya Gujarkar <gujarkaraditya@gmail.com>
2026-02-07 02:58:39 +0530

    completed react 3

--- react-states/src/Counter.jsx
+++ react-states/src/Counter.jsx
@@ -2,11 +2,22 @@
 import { useState } from "react";
 
 export default function Counter(){
-    let [count,setCount]=useState(0);
+    let [count,setCount]=useState(0);  // initialiation step
 
-    function increaseCount(){
-        setCount(count+1);
-        console.log(count); // this will log the old value of count because state updates are asynchronous
+    // SIMPLE COUNTER CODE
+    // function increaseCount(){
+    //     setCount(count+1);
+    //     console.log(count); // this will log the old value of count because state updates are asynchronous
+    // }
+
+    // CALLBACK IN UPDATER FUNCTION
+    let increaseCount=()=>{
+        setCount((currCount)=>{
+            return currCount+1
+        });
+        setCount((currCount)=>{
+            return currCount+1
+        });
     }
 
     return(
